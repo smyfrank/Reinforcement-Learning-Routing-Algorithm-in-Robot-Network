b063a4175511dc8c2d00a10c6e29a52d6548d3b0
smyfrank <smy1997@hotmail.com>
2021-03-12 18:57:02 +0800

    add random waypoint position generator, record trajectory of nodes

--- Mobility.py
+++ Mobility.py
@@ -1,6 +1,27 @@
 from pymobility.models.mobility import random_waypoint
+import copy
 
 '''This file add mobility to the nodes of the network'''
+
+
 class Mobility(object):
-    def __init__(self, network):
-        randomWaypoint = random_waypoint(network.nnodes, dimensions=(1, 1), velocity=(network.minSpeed, network.maxSpeed), wt_max=1.0)
+    def __init__(self, mobility_model, nnodes, min_speed, max_speed):
+        self.trajectory = {}  # record trajectory of all nodes.
+        self.step = 0    # record steps
+        self.node_number = nnodes
+        if mobility_model == 'random_waypoint':  # instance of mobility model, actually a iter generator
+            self.mb = random_waypoint(nnodes, dimensions=(1, 1), velocity=(min_speed, max_speed), wt_max=1.0)
+        else:
+            print('Undefined mobility model')
+
+    def get_next_way_point(self):
+        positions = next(self.mb)
+
+        # record trajectory of nodes
+        for i in range(self.node_number):
+            self.trajectory.setdefault(i, []).append(positions[i])
+
+        return positions
+
+    def print_trajectory(self):
+        print(self.trajectory)
