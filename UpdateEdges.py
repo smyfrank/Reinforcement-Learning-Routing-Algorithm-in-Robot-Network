cdf3f178c681532c029b5f56f9d5ceb43a91b231
smyfrank <smy1997@hotmail.com>
2021-03-16 16:27:03 +0800

    add calculate_nodes_connection() which add edges or remove edges by current position of nodes

--- UpdateEdges.py
+++ UpdateEdges.py
@@ -16,12 +16,14 @@ def Delete(dyNetwork, min_edge_removal, max_edge_removal):
     dyNetwork._network.remove_edges_from(strip)
     dyNetwork._stripped_list.extend(strip)
 
+
 ''' Randomly restores some edges we have deleted '''
 def Restore(dyNetwork):
     restore_number = random.randint(0, len(dyNetwork._stripped_list))
     restore = random.choices(dyNetwork._stripped_list, k=restore_number)
     dyNetwork._network.add_edges_from(restore)
-    
+
+
 ''' Randomly change edge weights '''
 # edge weight is actually edge delay
 def Random_Walk(dyNetwork):
@@ -31,13 +33,15 @@ def Random_Walk(dyNetwork):
             dyNetwork._network[s_edge][e_edge]['edge_delay'] = max(changed, 1)
         except:
             print(s_edge, e_edge)
-            
+
+
 ''' Change edge weights so that the edge weight changes will be roughly sinusoidal across the simulation '''
 # Why sine_state step is pi/6? Maybe it is just a super parameter.
 def Sinusoidal(dyNetwork):
     for s_edge, e_edge in dyNetwork._network.edges():
         dyNetwork._network[s_edge][e_edge]['edge_delay'] = max(1, int(dyNetwork._network[s_edge][e_edge]['initial_weight']* (1 + 0.5 * math.sin(dyNetwork._network[s_edge][e_edge]['sine_state']))))
         dyNetwork._network[s_edge][e_edge]['sine_state'] += math.pi/6
+
 
 ''' Not in use. If it were used the edge weight would be the average of the number of packets in each queue of the endpoints of the edge. '''
 def Average(dyNetwork):
@@ -48,4 +52,15 @@ def Average(dyNetwork):
         dyNetwork._network[node1][node2]['edge_delay'] = avg
         del tot_queue1, tot_queue2
 
-''' Judge whether two nodes are in connection depending on the positions and communication range'''
+
+''' Judge whether two nodes are in connection depending on the positions and communication radius'''
+def calculate_nodes_connection(dyNetwork, radius):
+    for i in range(dyNetwork.nnodes):
+        for j in range(i, dyNetwork.nnodes):
+            if i != j:
+                pos1 = dyNetwork._network.nodes[i]['pos']
+                pos2 = dyNetwork._network.nodes[j]['pos']
+                if math.sqrt(math.pow((pos1[0]-pos2[0]), 2) +math.pow((pos1[1]-pos2[1]), 2)) <= radius:
+                    dyNetwork._network.add_edge(i, j)
+                else:
+                    dyNetwork._network.remove_edge(i, j)
